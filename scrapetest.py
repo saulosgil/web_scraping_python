6a63ed0e0979463fe39f97fba200cc994607a5af
saulosgil <saulosgil@hotmail.com>
2024-01-06 11:06:18 -0300

    scrapetest

---
+++ scrapetest.py
@@ -0,0 +1,12 @@
+
+'''
+First scraping test - 
+Page - http://pythonscraping.com/pages/page1.html
+'''
+
+# Bib
+from urllib.request import urlopen
+
+# Getting url and printing 
+html = urlopen(url='http://pythonscraping.com/pages/page1.html')
+print(html.read())
